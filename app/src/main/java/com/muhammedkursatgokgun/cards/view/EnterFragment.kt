d4238e7ee1cebef5f1315cd6bed60c008ca8a960
Muhammed <kgokgun94@gmail.com>
2023-06-21 05:45:19 +0300

    Project created, layouts fragments navigation are created and modified.

---
+++ app/src/main/java/com/muhammedkursatgokgun/cards/view/EnterFragment.kt
@@ -0,0 +1,23 @@
+package com.muhammedkursatgokgun.cards.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muhammedkursatgokgun.cards.R
+
+class EnterFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_enter, container, false)
+    }
+}
