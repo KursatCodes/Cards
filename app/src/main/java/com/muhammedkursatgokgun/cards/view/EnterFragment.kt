d65fed031cd61ce7943ff9ba22b36500ee98437f
Muhammed <kgokgun94@gmail.com>
2023-06-22 08:12:20 +0300

    First action is ok.

--- app/src/main/java/com/muhammedkursatgokgun/cards/view/EnterFragment.kt
+++ app/src/main/java/com/muhammedkursatgokgun/cards/view/EnterFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.muhammedkursatgokgun.cards.R
+import com.muhammedkursatgokgun.cards.databinding.FragmentEnterBinding
 
+private var _binding: FragmentEnterBinding? = null
+// This property is only valid between onCreateView and
+// onDestroyView.
+private val binding get() = _binding!!
 class EnterFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +23,17 @@ class EnterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_enter, container, false)
+        _binding = FragmentEnterBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+        //return inflater.inflate(R.layout.fragment_enter, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonWork.setOnClickListener {
+            val action = EnterFragmentDirections.actionEnterFragmentToWordFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
