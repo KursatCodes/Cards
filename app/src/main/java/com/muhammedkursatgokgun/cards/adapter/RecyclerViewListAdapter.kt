14b1e099f10c7f78220a4da56698b6511377fa54
Muhammed <kgokgun94@gmail.com>
2023-07-22 21:22:39 +0300

    ListFragment added, recyclerview work with no problem.

--- app/src/main/java/com/muhammedkursatgokgun/cards/adapter/RecyclerViewListAdapter.kt
+++ app/src/main/java/com/muhammedkursatgokgun/cards/adapter/RecyclerViewListAdapter.kt
@@ -1,23 +1,28 @@
 package com.muhammedkursatgokgun.cards.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.muhammedkursatgokgun.cards.databinding.RecyclerRowBinding
+import com.muhammedkursatgokgun.cards.model.Word
 
-class RecyclerViewListAdapter : RecyclerView.Adapter<RecyclerViewListAdapter.WordHolder>() {
-    class WordHolder(view : View) : RecyclerView.ViewHolder(view){
+class RecyclerViewListAdapter(var wordList : List<Word>) : RecyclerView.Adapter<RecyclerViewListAdapter.WordHolder>() {
+    class WordHolder(var binding: RecyclerRowBinding) : RecyclerView.ViewHolder(binding.root){
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordHolder {
-        TODO("Not yet implemented")
+        val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return WordHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return wordList.size
     }
 
     override fun onBindViewHolder(holder: WordHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding.englishWord.text = wordList.get(position).englishW
+        holder.binding.turkishWord.text = wordList.get(position).turkW
     }
 }
--- app/src/main/java/com/muhammedkursatgokgun/cards/view/ListFragment.kt
+++ app/src/main/java/com/muhammedkursatgokgun/cards/view/ListFragment.kt
@@ -5,17 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.muhammedkursatgokgun.cards.R
-import com.muhammedkursatgokgun.cards.databinding.FragmentAdderBinding
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.muhammedkursatgokgun.cards.adapter.RecyclerViewListAdapter
 import com.muhammedkursatgokgun.cards.databinding.FragmentListBinding
+import com.muhammedkursatgokgun.cards.model.Word
+import com.muhammedkursatgokgun.cards.roomdb.WordDao
+import com.muhammedkursatgokgun.cards.roomdb.WordDb
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
+private lateinit var myAdapter : RecyclerViewListAdapter
+private lateinit var db : WordDb
+private lateinit var myDao : WordDao
+private lateinit var myWordList : List<Word>
+
+private var myDisposable = CompositeDisposable()
 private var _binding: FragmentListBinding? = null
 private val binding get() = _binding!!
 
 class ListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        db = Room.databaseBuilder(requireContext(),WordDb::class.java,"Word").build()
+        myDao = db.wordDao()
+
+
+
+
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +52,16 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        myDisposable.add(myDao.getAll()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse)
+        )
     }
+    private fun handleResponse(wordList : List<Word>){
+        binding.recyclerViewList.layoutManager = LinearLayoutManager(requireContext())
+        myAdapter = RecyclerViewListAdapter(wordList)
+        binding.recyclerViewList.adapter = myAdapter
+    }
+
 }
