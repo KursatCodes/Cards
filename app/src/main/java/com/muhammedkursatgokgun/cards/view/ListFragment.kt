948919771c1cc568136da6efecb72578757b2aa4
Muhammed <kgokgun94@gmail.com>
2023-07-23 15:54:59 +0300

    Google Ads added.

--- app/src/main/java/com/muhammedkursatgokgun/cards/view/ListFragment.kt
+++ app/src/main/java/com/muhammedkursatgokgun/cards/view/ListFragment.kt
@@ -20,15 +20,22 @@ import com.muhammedkursatgokgun.cards.roomdb.WordDb
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.muhammedkursatgokgun.cards.R
 
 private lateinit var myAdapter : RecyclerViewListAdapter
 private lateinit var db : WordDb
 private lateinit var myDao : WordDao
 private lateinit var myWordList : List<Word>
 
+
+
 private var myDisposable = CompositeDisposable()
 private var _binding: FragmentListBinding? = null
 private val binding get() = _binding!!
+lateinit var mAdView : AdView
 
 class ListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,6 +65,13 @@ class ListFragment : Fragment() {
             var action= ListFragmentDirections.actionListFragmentToEnterFragment()
             Navigation.findNavController(it).navigate(action)
         }
+        MobileAds.initialize(requireContext()){
+            // Ad ID                ca-app-pub-5968740567269412/7613511221
+            // banner test ID   ca-app-pub-3940256099942544/6300978111
+        }
+        mAdView = view.findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
     private fun handleResponse(wordList : List<Word>){
         binding.recyclerViewList.layoutManager = LinearLayoutManager(requireContext())
